8e64d6846d73f44ee7b37a4cd91b34f896850995
KhezamiTaha <khezamitaha10@gmail.com>
2024-03-08 14:27:48 +0000

    Update console and fileStorage to deal with Base_Model children

--- console.py
+++ console.py
@@ -2,7 +2,12 @@
 
 import cmd
 import shlex
-from models.base_model import BaseModel
+from models.state import BaseModel
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.place import Place
 from models.user import User
 from models import storage
 
